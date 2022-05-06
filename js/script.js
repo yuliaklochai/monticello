9a9147152af0ae03145992b4a73aa2f9c8274f63
yuliaklochai <klochay77@gmail.com>
2022-05-06 16:16:01 +0300

    add slider

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$('.banner__slider').slick({
+    infinity: true,
+    arrows: false,
+    dots: true,
+    vertical: true,
+    verticalSwiping: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
+});
